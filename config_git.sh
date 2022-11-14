b52093c7397fa09feb72a2dc70e8358c1c05c4db
vvcjw <vvcjw@kakao.com>
2022-11-14 22:17:30 +0900

    config init

---
+++ config_git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+git config --global log.date iso
+git config --global core.quotepath false
+git config --global alias.ll "log \
+    --date-order \
+    --date=iso \
+    --graph \
+    --full-history \
+    --all \
+    --pretty=format:'%x08%x09%C(red)%h %C(cyan)%ad%x08%x08%x08%x08%x08%x08 %C(bold blue)%aN%C(reset)%C(bold yellow)%d %C(reset)%s'"
