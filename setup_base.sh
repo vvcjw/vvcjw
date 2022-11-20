c2cd76229128da7c21c9cf0e7f138e9c0d9da004
vvcjw <vvcjw@kakao.com>
2022-11-20 18:54:15 +0900

    modified alias vvcjw

--- setup_base.sh
+++ setup_base.sh
@@ -7,7 +7,7 @@ echo '# vvcjw environment variable' >> ~/.profile
 echo 'export VVCJW_PATH="/home/$USER/.vvcjw"' >> ~/.profile
 echo 'export PATH="$VVCJW_PATH/bin:$PATH"' >> ~/.profile
 echo '' >> ~/.profile
-echo 'alias vvcjw="cd ${VVCJW_PATH}" | source $VVCJW_PATH/venv/bin/activate' >> ~/.profile
+echo 'alias vvcjw="cd $VVCJW_PATH && source $VVCJW_PATH/venv/bin/activate"' >> ~/.profile
 echo '' >> ~/.profile
 echo 'source $VVCJW_PATH/prompt_git.sh' >> ~/.profile
 echo '' >> ~/.profile
