c5d602b8436f5870524fcd2b76b7c8f73f1038f8
vvcjw <vvcjw@kakao.com>
2022-12-11 00:03:19 +0900

    test

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 
 
-git clone --single-branch https://github.com/vvcjw/vvcjw.git ~/.vvcjw
+
+ROOT_DIR="~/.vvcjw"
+if [ -d "$DIR" ]; then
+    echo ".vvcjw exists."
+else
+    git clone --single-branch https://github.com/vvcjw/vvcjw.git ~/.vvcjw
+fi
+
+
